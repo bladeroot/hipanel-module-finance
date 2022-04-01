65bc1aa2e9879dd09412736aa3b42c142e64333c
Yurii Myronchuk <bladeroot@hiqdev.com>
2022-04-01 14:32:30 +0000

    changes

--- src/logic/bill/SupportTimeQuantity.php
+++ src/logic/bill/SupportTimeQuantity.php
@@ -21,8 +21,10 @@ class SupportTimeQuantity extends DefaultQuantityFormatter
 {
     public function format(): string
     {
-        return Yii::t('hipanel:finance', '{quantity}', [
-            'quantity' => Yii::$app->formatter->asDuration(ceil($this->getQuantity()->getQuantity() * 3600))
+        return Yii::t('hipanel:finance', '{hours}:{minutes}', [
+            'quantity' => Yii::$app->formatter->asDuration($this->getQuantity()->getQuantity() * 3600),
+            'hours' => floor($this->getQuantity()->getQuantity()),
+            'minutes' => Yii::$app->formatter->asTime($this->getQuantity()->getQuantity() * 3600, 'mm'),
         ]);
     }
 }
